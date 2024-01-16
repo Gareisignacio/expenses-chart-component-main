cac2aedd463db8ac228d1a0b3cff461c84881e66
Gareisignacio <93623301+Gareisignacio@users.noreply.github.com>
2024-01-16 18:32:39 -0300

    script

--- app.js
+++ app.js
@@ -30,5 +30,28 @@ const data = [
  ];
 
 for (let i = 0; i < data.length; i++){
-   
+  let graphBox = document.querySelector(".graphs-box")
+  let dayDiv = document.createElement('div');
+  dayDiv.classList.add('day');
+
+  let dayAmount = document.createElement('p')
+  dayAmount.classList.add(".day-amount");
+
+  let dayPara = document.createElement('p');
+
+  let graphDiv = document.createElement('div');
+  graphDiv.classList.add(".graph");
+
+  dayAmount.textContent = data[i]["amount"];
+  dayPara.textContent = data[i]['day'];
+
+  let graphHeight = (data[i]['amount'] * 3);
+  
+  graphDiv.style = `height: ${graphHeight}`
+
+  dayDiv.appendChild(dayAmount);
+  dayDiv.appendChild(graphDiv);
+  dayDiv.appendChild(dayPara);
+
+  graphBox.appendChild(dayDiv)
 }
